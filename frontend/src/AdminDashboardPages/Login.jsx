15dc99e6b15f4ec2a822d081c9cca9bf995330c7
Azeez-Abiola <Abiolaquadri111@gmail.com>
2025-05-27 09:40:49 +0100

    authcontext

--- frontend/src/AdminDashboardPages/Login.jsx
+++ frontend/src/AdminDashboardPages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 import { Eye, EyeOff, Mail, Lock } from 'lucide-react';
 
 const Login = () => {
